a8e20a429bfaabeaea167e6dc12aabb5413319aa
Om Agarwalla <omagarwalla22@gmail.com>
2026-05-14 02:24:51 +0530

    Create App class with main method and sum function

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from DevOps Lab!");
+        System.out.println("Build and Deploy Pipeline Running!");
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
